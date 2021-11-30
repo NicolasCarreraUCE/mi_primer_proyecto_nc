3461ce8738192b85c5619db996873e7a3e573199
USUARIO <USUARIO@DESKTOP-NJCV4OK>
2021-11-29 22:08:17 -0500

    Primera subida del proyecto

---
+++ src/main/java/com/proyecto/Animal.java
@@ -0,0 +1,11 @@
+package com.proyecto;
+
+public class Animal {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Proyecto en GitHub");
+
+	}
+
+}
---
+++ src/main/java/mi/primer/proyecto/nc/Animal.java
@@ -0,0 +1,10 @@
+package mi.primer.proyecto.nc;
+
+public class Animal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My primer proyecto en GitHub...");
+	}
+
+}
